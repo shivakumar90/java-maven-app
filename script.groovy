1966275383eb93c93d6129c85d41fa0e91672995
shivakumar90 <118954702+shivakumar90@users.noreply.github.com>
2025-05-22 17:27:45 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,9 +6,9 @@ def buildJar() {
 def buildImage() {
     echo "building the application..."
                     withCredentials([usernamePassword(credentialsId: 'dockerhub-repo', usernameVariable: "USER", passwordVariable: "PASS")]) {
-                        sh "docker build -t shivakumarreddy1/demo-app:jma-2.0 ."
+                        sh "docker build -t shivakumarreddy1/demo-app:jma-3.0 ."
                         sh "echo $PASS | docker login -u $USER --password-stdin"
-                        sh "docker push shivakumarreddy1/demo-app:jma-2.0"
+                        sh "docker push shivakumarreddy1/demo-app:jma-3.0"
                     }
 } 
 
